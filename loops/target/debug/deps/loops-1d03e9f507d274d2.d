b1ce5b1b952815d530663a4290b9df58a7877609
DapperBison <alexandersutton1999@gmail.com>
2025-09-03 11:01:00 -0600

    learned about loops, adding to repo

---
+++ loops/target/debug/deps/loops-1d03e9f507d274d2.d
@@ -0,0 +1,5 @@
+/home/alex/Desktop/redRust/loops/target/debug/deps/loops-1d03e9f507d274d2.d: src/main.rs
+
+/home/alex/Desktop/redRust/loops/target/debug/deps/loops-1d03e9f507d274d2: src/main.rs
+
+src/main.rs:
---
+++ loops/target/debug/loops.d
@@ -0,0 +1,1 @@
+/home/alex/Desktop/redRust/loops/target/debug/loops: /home/alex/Desktop/redRust/loops/src/main.rs
